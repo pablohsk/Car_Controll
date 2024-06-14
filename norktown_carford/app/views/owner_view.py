0692383954f08c6f9deb143302da13cd99335836
Pablohsk <pablofidelisdias@gmail.com>
2024-06-14 11:55:17 -0300

    Adicionadas rotas em owner_view

--- norktown_carford/app/views/owner_view.py
+++ norktown_carford/app/views/owner_view.py
@@ -1,10 +1,37 @@
 from flask import Blueprint, request, jsonify
-from ..controllers.owner_controller import create_owner
+from flask_jwt_extended import jwt_required
+from ..controllers.owner_controller import create_owner, get_owner, update_owner, delete_owner
+from ..dtos.owner_dto import OwnerDTO
 
 owner_bp = Blueprint('owners', __name__)
 
 @owner_bp.route('', methods=['POST'])
+@jwt_required()
 def add_owner():
     data = request.get_json()
     new_owner = create_owner(data)
-    return jsonify({'id': new_owner.id}), 201
+    return jsonify(OwnerDTO(new_owner).serialize()), 201
+
+@owner_bp.route('/<int:owner_id>', methods=['GET'])
+@jwt_required()
+def get_owner_route(owner_id):
+    owner = get_owner(owner_id)
+    if owner:
+        return jsonify(OwnerDTO(owner).serialize())
+    return jsonify({'error': 'Owner not found'}), 404
+
+@owner_bp.route('/<int:owner_id>', methods=['PUT'])
+@jwt_required()
+def update_owner_route(owner_id):
+    data = request.get_json()
+    updated_owner = update_owner(owner_id, data)
+    if updated_owner:
+        return jsonify(OwnerDTO(updated_owner).serialize())
+    return jsonify({'error': 'Owner not found'}), 404
+
+@owner_bp.route('/<int:owner_id>', methods=['DELETE'])
+@jwt_required()
+def delete_owner_route(owner_id):
+    if delete_owner(owner_id):
+        return jsonify({'message': 'Owner deleted'}), 200
+    return jsonify({'error': 'Owner not found'}), 404
