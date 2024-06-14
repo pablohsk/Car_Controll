04cf81f383b612c2e4a35aaed8edec765002d921
Pablohsk <pablofidelisdias@gmail.com>
2024-06-14 11:51:50 -0300

    Criado código para owner_dto

---
+++ norktown_carford/app/dtos/owner_dto.py
@@ -0,0 +1,12 @@
+class OwnerDTO:
+    def __init__(self, owner):
+        self.id = owner.id
+        self.name = owner.name
+        self.cars = [CarDTO(car).serialize() for car in owner.cars]
+
+    def serialize(self):
+        return {
+            'id': self.id,
+            'name': self.name,
+            'cars': self.cars
+        }
