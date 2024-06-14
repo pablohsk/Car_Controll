fffdd5ab07270f730ce36c7dfeaa77ba04efb7d7
Pablohsk <pablofidelisdias@gmail.com>
2024-06-14 11:54:58 -0300

    Adicionadas rotas em car_view

--- norktown_carford/app/views/car_view.py
+++ norktown_carford/app/views/car_view.py
@@ -1,6 +1,7 @@
 from flask import Blueprint, request, jsonify
-from flask_jwt_extended import jwt_required, create_access_token
-from ..controllers.car_controller import create_car
+from flask_jwt_extended import jwt_required
+from ..controllers.car_controller import create_car, get_car, update_car, delete_car
+from ..dtos.car_dto import CarDTO
 
 car_bp = Blueprint('cars', __name__)
 
@@ -10,11 +11,29 @@ def add_car():
     data = request.get_json()
     new_car = create_car(data)
     if new_car:
-        return jsonify({'id': new_car.id}), 201
+        return jsonify(CarDTO(new_car).serialize()), 201
     return jsonify({'error': 'Owner not found or car limit reached'}), 400
 
-@car_bp.route('/login', methods=['POST'])
-def login():
+@car_bp.route('/<int:car_id>', methods=['GET'])
+@jwt_required()
+def get_car_route(car_id):
+    car = get_car(car_id)
+    if car:
+        return jsonify(CarDTO(car).serialize())
+    return jsonify({'error': 'Car not found'}), 404
+
+@car_bp.route('/<int:car_id>', methods=['PUT'])
+@jwt_required()
+def update_car_route(car_id):
     data = request.get_json()
-    access_token = create_access_token(identity=data['username'])
-    return jsonify(access_token=access_token), 200
+    updated_car = update_car(car_id, data)
+    if updated_car:
+        return jsonify(CarDTO(updated_car).serialize())
+    return jsonify({'error': 'Car not found'}), 404
+
+@car_bp.route('/<int:car_id>', methods=['DELETE'])
+@jwt_required()
+def delete_car_route(car_id):
+    if delete_car(car_id):
+        return jsonify({'message': 'Car deleted'}), 200
+    return jsonify({'error': 'Car not found'}), 404
